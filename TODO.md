de67d1861eb0dee62b198d78ec4ab4342d27ad06
Adam Taylor <adamjctaylor@gmail.com>
2011-12-21 12:48:58 +0000

    Work so far

---
+++ TODO.md
@@ -0,0 +1,19 @@
+TODO
+----
+
+Core
+====
+
+* Youtube search
+* Add to playlist
+* Player
+
+Extras
+======
+
+* Related artists
+* Show/hide search box (AJAXY)
+* Voting for/against songs
+* Bot::BasicBot::Pluggable::Module::DemocraDJ
+* SQLite data store
+* Scrobble
--- lib/Democradj.pm
+++ lib/Democradj.pm
@@ -4,4 +4,37 @@ use strict;
 use warnings;
 use Dancer;
 
-1;
+get '/' => sub {
+    template 'index';
+};
+
+get '/api/playlist' => sub {
+    return [
+        {
+            0 => {
+                src => 'http://www.youtube.com/watch?v=ZdUINbi4wSY',
+                type => 'video/youtube'
+            }
+        },
+        {
+            0 => {
+                src => 'http://www.youtube.com/watch?v=GW1NTN5vMyY',
+                type => 'video/youtube'
+            }
+        },
+        {
+            0 => {
+                src => 'http://www.youtube.com/watch?v=lj39bxQC_VU',
+                type => 'video/youtube'
+            }
+        },
+        {
+            0 => {
+                src => '/api/playlist',
+                type => 'text/json'
+            },
+        },
+    ];
+};
+
+dance;
