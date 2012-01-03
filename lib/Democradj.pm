0f35a20c4f53e91019f218cd08752d86772e9588
Adam Taylor <adamjctaylor@gmail.com>
2012-01-03 22:31:49 +0000

    Start adding the youtube serch functionality

--- lib/Democradj.pm
+++ lib/Democradj.pm
@@ -2,7 +2,9 @@ package Democradj;
 
 use strict;
 use warnings;
+use LWP::UserAgent;
 use Dancer;
+use JSON;
 
 get '/' => sub {
     template 'index';
@@ -35,6 +37,28 @@ get '/api/playlist' => sub {
             },
         },
     ];
+};
+
+post '/search' => sub {
+    my $query = params->{search};
+
+    my $client = LWP::UserAgent->new;
+    my $resp = $client->get( "https://gdata.youtube.com/feeds/api/videos?q=$query&orderby=rating&max-results=50&v=2&alt=json" );
+
+    my @ret;
+    if ( $resp->is_success ) {
+
+        my $ref = decode_json $resp->content;
+
+        foreach ( @{$ref->{feed}->{entry}} ) {
+            push @ret, {
+                url   => $_->{'media$group'}->{'media$player'}->{'url'},
+                title => $_->{'media$group'}->{'media$title'}->{'$t'},
+            };
+        }
+    }
+
+    return \@ret;;
 };
 
 dance;
